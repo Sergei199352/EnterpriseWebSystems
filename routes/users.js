2dea1e58c1b80cc413e0b307238e950dbd6cd273
sergey <s.stanislavchuk@rgu.ac.uk>
2024-03-16 21:00:59 +0000

    requiring flash

--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/Users')
-
+const catchAsync = require('../utils/catchAsync')
 router.get('/register', (req,res)=>{
     res.render('users/register')
 });
-router.post('/register', async (req,res) =>{
-    try {
+router.post('/register', catchAsync(async (req,res) =>{
+
         const { email, username, password } = req.body;
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
         console.log(registeredUser);
         res.redirect('/register');
-    } catch(e) {
-        console.log(e);
-        res.status(500).send("Error registering user");
-        res.redirect('/register')
-    }
-});
+    
+}));
 
 
 module.exports = router;
